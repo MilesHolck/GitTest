bfb6044f37d27b131287554dc459b0c97c7ccd49
camil <camilla-holck@hotmail.com>
2025-11-14 10:17:24 +0100

    branch test med en console WL :D

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -4,3 +4,5 @@ Console.WriteLine("Den her skal til Github :3");
 Console.WriteLine("Og denne her skal og til Github :D");
 
 Console.WriteLine("denne skal tilføjes :O");
+
+Console.WriteLine("denne linje tester om dette er en branch:)");
